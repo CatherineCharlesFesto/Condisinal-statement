c48f870e3ff6636b555601e770f609708e4fcc6c
Catherine <catherinecharlesfesto@gmail.com>
2025-07-25 19:13:36 +0300

    commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#write a program to check whether the given number is positive?
+
+num=int(input("plese enter a number"))
+if num<0:
+    print("Its a negative number")
+else:
+    print("Is a postive number")
