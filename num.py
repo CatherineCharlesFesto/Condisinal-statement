095c50f1c9e9ad3f5bee911ba1d52032b7385bab
Catherine <catherinecharlesfesto@gmail.com>
2025-07-25 19:33:22 +0300

    3 commit

---
+++ num.py
@@ -0,0 +1,9 @@
+#Write a program to check whether the given number is greater than 15 or smaller than 15.
+
+num=int(input("enter any numbe to chek if its gretter than 15 or smaller than 15"))
+if num>15:
+    print(f"{num} is grater than 15")
+elif num<15:
+    print(f"{num} is smaller than 15")
+else:
+    print(f"{num} is equal to 15")
